5c49c55c027028fb1734b8566dfbb10608fed229
karmen <166833697+LuciusKarmen@users.noreply.github.com>
2025-10-20 13:18:10 +0800

    10.20.2

--- frontend/Star-vue/src/pages/Main/main.vue
+++ frontend/Star-vue/src/pages/Main/main.vue
@@ -1,10 +1,8 @@
 <template>
-  <div>
-    <div class="header">
-      <div class="title"></div>
-      <div class="classic"></div>
-    </div>
-    <div>
+  <div class="main">
+    <div class="header"></div>
+    <div class="classic"></div>
+    <div class="top_v">
       <!-- 设计轮播图 -->
       <div></div>
       <!-- 推荐 -->
@@ -17,4 +15,18 @@
 <script lang="ts" setup name="">
 import { ref } from 'vue'
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.main {
+  width: 100%;
+  .header {
+    width: 100%;
+    height: 170px;
+    background-color: aqua;
+  }
+  .classic {
+    width: 100%;
+    height: 100px;
+    background-color: rgb(216, 254, 78);
+  }
+}
+</style>
