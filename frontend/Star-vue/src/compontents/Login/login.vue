243e07b0b3f55538439432b071769e31ea070375
karmen <166833697+LuciusKarmen@users.noreply.github.com>
2025-10-28 23:59:21 +0800

    10.28.2

--- frontend/Star-vue/src/compontents/Login/login.vue
+++ frontend/Star-vue/src/compontents/Login/login.vue
@@ -3,13 +3,47 @@
   <!-- 遮罩层，点击可关闭 -->
   <div class="login-overlay" v-if="show" @click="close">
     <div class="login-box" @click.stop>
-      <div class="title">登录\注册</div>
-      <div class="right">
-        <div>邮箱</div>
-        <div>密码</div>
-        <div>验证码</div>
-        <button>确认</button>
+      <div class="login_form">
+        <div class="title">
+          <div class="login">登录</div>
+          <div class="reg">注册</div>
+        </div>
+        <div class="form">
+          <el-form>
+            <el-form-item label="账号">
+              <el-input placeholder="请输入用户名/邮箱"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+              <el-input placeholder="请输入密码"></el-input>
+            </el-form-item>
+            <el-form-item label="验证码">
+              <el-input placeholder="请输入验证码"></el-input>
+              <div>1</div>
+            </el-form-item>
+            <el-button>清空</el-button>
+            <el-button type="primary">确认</el-button>
+          </el-form>
+        </div>
       </div>
+      <!-- 两个部分 -->
+      <!-- <div class="reg_form">
+        <el-form>
+          <el-form-item label="账号">
+            <el-input placeholder="请输入用户名"></el-input>
+          </el-form-item>
+          <el-form-item label="密码">
+            <el-input placeholder="请输入密码"></el-input>
+          </el-form-item>
+          <el-form-item label="确认密码">
+            <el-input placeholder="请确认密码"></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱">
+            <el-input placeholder="请输入邮箱"></el-input>
+            <button>发送</button>
+          </el-form-item>
+          <el-form-item label="验证码"> </el-form-item>
+        </el-form>
+      </div> -->
     </div>
   </div>
 </template>
@@ -53,7 +87,37 @@ const close = () => {
     background-image: url('../../../public/姐妹.jpg');
     background-size: cover;
     background-position: center;
-    // 背景图片半透明
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    position: relative;
+    .login_form {
+      position: absolute;
+      top: 10%;
+      left: 30%;
+      width: 40%;
+      height: 70%;
+      display: flex;
+      flex-direction: column;
+      background-color: rgba($color: #e9e9e9, $alpha: 0.8);
+      border: 2px solid rgb(255, 255, 255);
+      border-radius: 10px;
+      .title {
+        display: flex;
+        justify-content: space-around;
+        font-size: 3vh;
+      }
+      .form {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        .el-input {
+          width: 80%;
+          height: 5vh;
+          margin: 0 auto;
+        }
+      }
+    }
   }
 }
 </style>
