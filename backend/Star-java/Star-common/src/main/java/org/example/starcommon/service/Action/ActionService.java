b6db334c7bf184e84982e761e058922e7bdd150c
karmen <166833697+LuciusKarmen@users.noreply.github.com>
2025-11-29 23:13:04 +0800

    11.29

---
+++ backend/Star-java/Star-common/src/main/java/org/example/starcommon/service/Action/ActionService.java
@@ -0,0 +1,11 @@
+package org.example.starcommon.service.Action;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ActionService {
+    @Autowired
+    
+
+}
